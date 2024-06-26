38bf1c397ad845ff1fa5d585b0099247b55237ce
Chris Venter <christiaan.venter@infrasonic.co.za>
2024-06-26 16:10:25 +0200

    Move User Logic into Controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,156 @@
+import { generateToken } from "../utils/jwtUtil";
+import { encryptPassword, isCorrectPassword } from "../utils/passwordUtil";
+import { isValidEmail, validatePasswordStrength } from "../utils/regexUtil";
+
+export interface UserCredentials {
+  emailAddress: string;
+  password: string;
+  jwtToken?: string;
+}
+
+export async function registerUser(
+  fastify: any,
+  request: any,
+  reply: any
+): Promise<any> {
+  const { emailAddress, password }: UserCredentials =
+    request.body as UserCredentials;
+
+  const validatedPassword = validatePasswordStrength(password);
+
+  if (!validatedPassword.isStrong) {
+    return reply.status(400).send({
+      error: "Weak password",
+      message: validatedPassword.message,
+    });
+  }
+
+  if (!isValidEmail(emailAddress)) {
+    return reply.status(400).send({
+      error: "Invalid email",
+      message: "Email address is not valid",
+    });
+  }
+
+  try {
+    const existingUser = await findUser(fastify, emailAddress);
+
+    if (existingUser.length) {
+      return reply.status(400).send({
+        error: "User already exists",
+        message: "User email address already registered",
+      });
+    }
+
+    await addUser(fastify, emailAddress, password).then(() => {
+      reply.status(200).send({ message: "User registration successful" });
+    });
+  } catch (error: any) {
+    reply
+      .status(400)
+      .send({ error: "User registration failed", message: error.message });
+  }
+}
+
+export async function loginUser(
+  fastify: any,
+  request: any,
+  reply: any
+): Promise<any> {
+  const { emailAddress, password }: UserCredentials =
+    request.body as UserCredentials;
+
+  try {
+    const existingUser = await findUser(fastify, emailAddress);
+
+    if (!existingUser.length) {
+      return reply.status(400).send({
+        error: "User not found",
+        message: `User with email ${emailAddress} does not exist`,
+      });
+    }
+
+    const hashedPassword: string = existingUser[0].password;
+    const isPasswordCorrect: boolean = await isCorrectPassword(
+      password,
+      hashedPassword
+    );
+
+    if (isPasswordCorrect) {
+      const jwtToken: string = generateToken(existingUser[0]);
+
+      await updateUserJwtToken(fastify, emailAddress, jwtToken).then(() => {
+        reply
+          .status(200)
+          .send({ message: "User login successful", token: jwtToken });
+      });
+    } else {
+      reply.status(400).send({
+        error: "Invalid password",
+        message: "Password is not correct",
+      });
+    }
+  } catch (error: any) {
+    reply
+      .status(400)
+      .send({ error: "User login failed", message: error.message });
+  }
+}
+
+async function findUser(fastify: any, emailAddress: string): Promise<any> {
+  return await new Promise<any>((resolve, reject) => {
+    fastify.mysql.query(
+      `SELECT * FROM munch_pos.Users WHERE emailAddress = ?`,
+      [emailAddress],
+      (error: any, result: any[]) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+}
+
+async function addUser(
+  fastify: any,
+  emailAddress: string,
+  password: string
+): Promise<void> {
+  return await new Promise<void>(async (resolve, reject) => {
+    const hashedPassword: string = await encryptPassword(password);
+
+    fastify.mysql.query(
+      `INSERT INTO munch_pos.Users (emailAddress, password) VALUES (?, ?);`,
+      [emailAddress, hashedPassword],
+      (error: any) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
+}
+
+async function updateUserJwtToken(
+  fastify: any,
+  emailAddress: string,
+  jwtToken: string
+): Promise<any> {
+  await new Promise<any>((resolve, reject) => {
+    fastify.mysql.query(
+      `UPDATE munch_pos.Users SET jwtToken = ? WHERE emailAddress = ?`,
+      [jwtToken, emailAddress],
+      (error: any, result: any[]) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { RouteShorthandOptions } from "fastify";
 import mysql from "@fastify/mysql";
 import { MySQLPool } from "@fastify/mysql";
 import * as dotenv from "dotenv";
-import { encryptPassword, isCorrectPassword } from "./utils/passwordUtil";
-import { generateToken, authenticateJWT } from "./utils/jwtUtil";
-import { validatePasswordStrength, isValidEmail } from "./utils/regexUtil";
+import { authenticateJWT } from "./utils/jwtUtil";
+import { registerUser, loginUser } from "./controllers/userController";
 
 dotenv.config();
 
@@ -16,12 +15,6 @@ declare module "fastify" {
   interface FastifyInstance {
     mysql: MySQLPool;
   }
-}
-
-export interface UserCredentials {
-  emailAddress: string;
-  password: string;
-  jwtToken?: string;
 }
 
 const userRouteOptions: RouteShorthandOptions = {
@@ -42,137 +35,12 @@ fastify.register(mysql, {
 });
 
 fastify.post("/users/register", userRouteOptions, async (request, reply) => {
-  const { emailAddress, password }: UserCredentials =
-    request.body as UserCredentials;
-  const validatedPassword = validatePasswordStrength(password);
-
-  if (!validatedPassword.isStrong) {
-    return reply.status(400).send({
-      error: "Weak password",
-      message: validatedPassword.message,
-    });
-  }
-
-  if (!isValidEmail(emailAddress)) {
-    return reply.status(400).send({
-      error: "Invalid email",
-      message: "Email address is not valid",
-    });
-  }
-
-  try {
-    const existingUser = await findUser(emailAddress);
-
-    if (existingUser.length) {
-      return reply.status(400).send({
-        error: "User already exists",
-        message: "User email address already registered",
-      });
-    }
-
-    await registerUser(emailAddress, password).then(() => {
-      reply.status(200).send({ message: "User registration successful" });
-    });
-  } catch (error: any) {
-    reply
-      .status(400)
-      .send({ error: "User registration failed", message: error.message });
-  }
+  await registerUser(fastify, request, reply);
 });
-
-async function findUser(emailAddress: string): Promise<any> {
-  return await new Promise<any>((resolve, reject) => {
-    fastify.mysql.query(
-      `SELECT * FROM munch_pos.Users WHERE emailAddress = ?`,
-      [emailAddress],
-      (error, result: any[]) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      }
-    );
-  });
-}
-
-async function registerUser(
-  emailAddress: string,
-  password: string
-): Promise<void> {
-  return await new Promise<void>(async (resolve, reject) => {
-    const hashedPassword: string = await encryptPassword(password);
-
-    fastify.mysql.query(
-      `INSERT INTO munch_pos.Users (emailAddress, password) VALUES (?, ?);`,
-      [emailAddress, hashedPassword],
-      (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      }
-    );
-  });
-}
 
 fastify.post("/users/login", userRouteOptions, async (request, reply) => {
-  const { emailAddress, password }: UserCredentials =
-    request.body as UserCredentials;
-
-  try {
-    const existingUser = await findUser(emailAddress);
-
-    if (!existingUser.length) {
-      return reply.status(400).send({
-        error: "User not found",
-        message: `User with email ${emailAddress} does not exist`,
-      });
-    }
-
-    const hashedPassword: string = existingUser[0].password;
-    const isPasswordCorrect: boolean = await isCorrectPassword(
-      password,
-      hashedPassword
-    );
-
-    if (isPasswordCorrect) {
-      const jwtToken: string = generateToken(existingUser[0]);
-
-      await updateUserJwtToken(emailAddress, jwtToken).then(() => {
-        reply
-          .status(200)
-          .send({ message: "User login successful", token: jwtToken });
-      });
-    } else {
-      reply.status(400).send({
-        error: "Invalid password",
-        message: "Password is not correct",
-      });
-    }
-  } catch (error: any) {
-    reply
-      .status(400)
-      .send({ error: "User login failed", message: error.message });
-  }
+  await loginUser(fastify, request, reply);
 });
-
-async function updateUserJwtToken(emailAddress: string, jwtToken: string) {
-  await new Promise<any>((resolve, reject) => {
-    fastify.mysql.query(
-      `UPDATE munch_pos.Users SET jwtToken = ? WHERE emailAddress = ?`,
-      [jwtToken, emailAddress],
-      (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      }
-    );
-  });
-}
 
 //TODO: WIP
 fastify.get(
